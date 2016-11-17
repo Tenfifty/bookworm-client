af530386945fc59b6f727f8aac04a7837cb87362
Kristoffer Röshammar <roshammar@gmail.com>
2016-11-17 15:05:42 +0100

    Add API foundation

---
+++ bookworm/__init__.py
@@ -0,0 +1,13 @@
+"""
+Bookworm API Client
+
+"""
+
+__title__ = 'bookworm'
+__version__ = '0.1.0'
+__author__ = 'Crawlica AB'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Crawlica AB'
+
+
+from .api import Bookworm
